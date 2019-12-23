3e9e9d962c3e5029a74dc79678c0417256e230c1
Bobby H <rhoffman103@gmail.com>
2019-12-23 13:58:04 -0500

    basic engrams rendering

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,59 @@
 "use strict";
-console.log('success!');
+var engramImages = [
+    'rare-engram.png',
+    'legendary-engram.png',
+    'prototype-engram.png',
+    'exotic-engram.png',
+    'uncommon-engram.png'
+];
+;
+var Engrams = /** @class */ (function () {
+    function Engrams() {
+        this.engrams = [];
+        this.randomizeEngramValues(engramImages, 12);
+        this.renderEngrams();
+    }
+    ;
+    Engrams.prototype.randomizeEngramValues = function (imgs, maxValue) {
+        var i = 1;
+        var remainingNums = [];
+        while (i <= maxValue) {
+            remainingNums.push(i);
+            i++;
+        }
+        ;
+        this.engrams = imgs.map(function (img) {
+            var randNum = Math.floor(Math.random() * remainingNums.length);
+            var newValue = remainingNums.splice(randNum, 1)[0];
+            return { imageName: img, value: newValue };
+        });
+    };
+    ;
+    Engrams.prototype.clickHandler = function (element) {
+        console.log(element.getAttribute('value'));
+    };
+    ;
+    Engrams.prototype.renderEngrams = function () {
+        var _this = this;
+        var engramsDiv = document.getElementById('engrams');
+        var engramsFragment = document.createDocumentFragment();
+        this.engrams.forEach(function (engram) {
+            var img = document.createElement('img');
+            img.setAttribute('class', 'clickable-engram engram-btn');
+            img.setAttribute('src', "./assets/images/" + engram.imageName);
+            img.setAttribute('alt', 'engram');
+            img.setAttribute('value', engram.value.toString());
+            img.addEventListener('click', function (event) {
+                _this.clickHandler(event.currentTarget);
+            });
+            engramsFragment.appendChild(img);
+        });
+        engramsDiv.innerHTML = '';
+        engramsDiv.appendChild(engramsFragment);
+    };
+    ;
+    return Engrams;
+}());
+;
+new Engrams();
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,61 @@
-console.log('success!');
+const engramImages = [
+    'rare-engram.png',
+    'legendary-engram.png',
+    'prototype-engram.png',
+    'exotic-engram.png',
+    'uncommon-engram.png'
+];
+
+interface Engram {
+    imageName: string;
+    value: number;
+};
+
+class Engrams {
+    engrams: Engram[] = [];
+
+    constructor() {
+        this.randomizeEngramValues(engramImages, 12);
+        this.renderEngrams();
+    };
+
+    randomizeEngramValues(imgs: string[], maxValue: number) {
+        let i = 1;
+        let remainingNums: number[] = [];
+
+        while (i <= maxValue) {
+            remainingNums.push(i);
+            i++;
+        };
+
+        this.engrams = imgs.map((img) => {
+            const randNum = Math.floor(Math.random() * remainingNums.length);
+            const newValue = remainingNums.splice(randNum, 1)[0];
+            return <Engram> { imageName: img, value: newValue };
+        });
+    };
+
+    clickHandler(element: HTMLElement) {
+        console.log(element.getAttribute('value'));
+    };
+
+    private renderEngrams() {
+        let engramsDiv = document.getElementById('engrams')! as HTMLDivElement;
+        let engramsFragment = document.createDocumentFragment();
+        this.engrams.forEach(engram => {
+            let img = document.createElement('img');
+            img.setAttribute('class', 'clickable-engram engram-btn');
+            img.setAttribute('src', "./assets/images/" + engram.imageName);
+            img.setAttribute('alt', 'engram');
+            img.setAttribute('value', engram.value.toString());
+            img.addEventListener('click',(event) => {
+                this.clickHandler(event.currentTarget! as HTMLElement);
+            });
+            engramsFragment.appendChild(img);
+        });
+        engramsDiv.innerHTML = '';
+        engramsDiv.appendChild(engramsFragment);
+    };
+};
+
+new Engrams();
